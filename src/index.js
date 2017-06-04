fe9b42dbf10a26449672c03dd9604cc11270f106
Jürgen Ratzenböck <jur@tractive.com>
2017-06-04 10:35:28 +0200

    Extract two arrays for calculating correlation coefficient
    * Get command success rates in own array.
    * Get app openeings in own array.

--- src/index.js
+++ src/index.js
@@ -4,12 +4,35 @@ var async = require('async');
 getAppUsageServerCommandStatistic();
 
 function getAppUsageServerCommandStatistic() {
-    async.waterfall([
-        findAppUsages,
-        findCommandSuccessRate
-    ], function (err, result) {
-        console.log(result);
+    findCommandSuccessRate(function(err, commandSuccessRates) {
+        findAppUsages(commandSuccessRates, function(err, appUsages) {
+           mergeArraysById(commandSuccessRates, appUsages, function(res1, res2) {
+              console.log('Cmd Success Rates\n\n');
+              res1.forEach(function(cmdSr) {
+                  console.log(cmdSr);
+              });
+              console.log('App openings\n\n');
+              res2.forEach(function(appU) {
+                  console.log(appU);
+              });
+           });
+        });
     });
+}
+
+function mergeArraysById(arr1, arr2, cb) {
+    var resultArr1 = [];
+    var resultArr2 = [];
+
+    for (var i = 0; i < arr1.length; i++) {
+        for (var j = 0; j < arr2.length; j++) {
+            if (arr1[i]._id.toString() === arr2[j]._id.toString()) {
+                resultArr1.push(Math.round(arr1[i].cmdSuccessRate * 1000) / 1000);
+                resultArr2.push(arr2[j].appOpenings);
+            }
+        }
+    }
+    cb(resultArr1, resultArr2);
 }
 
 function getRandomUserIds(cb) {
@@ -57,57 +80,54 @@ function queryClientIds(connection, cb) {
         .toArray(
             function (err, result) {
                 connection.close();
-                var clientIds = [];
-                result.forEach(function (client) {
-                    clientIds.push(client._id)
-                });
-                return cb(err, clientIds);
+                return cb(err, extractArrayContent(result));
             });
 }
 
-function findAppUsages(cb) {
-    async.parallel({
-        userIds: getRandomUserIds,
+function findAppUsages(commandSuccessRates, cb) {
+    async.series({
         clientIds: getClientIds
     }, function (err, result) {
         async.waterfall([
             async.apply(db.createConnection, 'tractivedb_metrics'),
-            async.apply(getAppUsages, result.userIds, result.clientIds)
+            async.apply(queryAppUsages, extractArrayContent(commandSuccessRates), result.clientIds)
         ], function (err, appUsages) {
-            //console.log(appUsages);
             cb(err, appUsages);
         });
     });
 }
 
-function getAppUsages(userIds, clientIds, connection, cb) {
+function queryAppUsages(userIds, clientIds, connection, cb) {
     connection.collection('mobile_events').aggregate([
         {
             '$match': {
                 user_id: {$in: userIds},
                 created_at: {
-                    $gte: new Date("2017-05-01T00:00:00.000Z")
-
+                    $gte: new Date("2017-05-01T00:00:00.000Z"),
+                    $lt: new Date("2017-06-01T00:00:00.000Z")
                 },
                 event: 'app_foreground',
                 client_id: {$in: clientIds}
             }
         },
-        {'$group': {_id: '$user_id', countEvents: {$sum: 1}}},
-        {'$match': {countEvents: {$gte: 1}}},
-        {'$limit': 1086}
+        {'$group': {_id: '$user_id', appOpenings: {$sum: 1}}},
+        {'$match': {appOpenings: {$gte: 1}}}
     ], function (err, result) {
         connection.close();
         return cb(err, result);
     });
 }
 
-function findCommandSuccessRate(appUsages, cb) {
-    async.waterfall([
-        async.apply(db.createConnection, 'tractivedb_metrics'),
-        async.apply(queryCommandSuccessRate, extractArrayContent(appUsages))
-    ], function (err, result) {
-        cb(err, result);
+function findCommandSuccessRate(cb) {
+    async.series({
+       userIds: getRandomUserIds
+    }, function (err, result) {
+        async.waterfall([
+            async.apply(db.createConnection, 'tractivedb_metrics'),
+            async.apply(queryCommandSuccessRate, result.userIds)
+        ], function (err, result) {
+            cb(err, result);
+        });
     });
 }
 
@@ -129,7 +149,8 @@ function queryCommandSuccessRate(userIds, connection, cb) {
                 }
             }
         },
-        {'$group': {_id: '$requested_by', cmdSuccessRate: {$avg: '$cmd_success_rate'}}}
+        {'$group': {_id: '$requested_by', cmdSuccessRate: {$avg: '$cmd_success_rate'}}},
+        {'$limit': 1086}
     ], function (err, result) {
         connection.close();
         cb(err, result);
