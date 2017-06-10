c0e775c9b3d4154fdc7a10c765a49ff93070e60f
Jürgen Ratzenböck <jur@tractive.com>
2017-06-10 17:29:59 +0200

    Extend statistical analysis
    * Add more mongo aggregations.
    * Refactor aggregation pipeline building.
    * Write results to csv file.

--- src/db.js
+++ src/db.js
@@ -1,6 +1,7 @@
 var MongoClient = require('mongodb').MongoClient;
 
 module.exports.createConnection = createConnection;
+module.exports.aggregate = aggregate;
 
 function createConnection(database, cb) {
     MongoClient.connect('mongodb://localhost:47018/' + database, function(err, db) {
@@ -8,5 +9,12 @@ function createConnection(database, cb) {
             throw err;
         }
         cb(null, db);
+    });
+}
+
+function aggregate(connection, collection, pipeline, cb) {
+    connection.collection(collection).aggregate(pipeline, function (err, result) {
+        connection.close();
+        cb(err, result);
     });
 }
--- src/index.js
+++ src/index.js
@@ -1,23 +1,47 @@
 var db = require('./db');
 var async = require('async');
+var fs = require('fs');
+var csvWriter = require('csv-write-stream');
 
-getAppUsageServerCommandStatistic();
+calcAppUsageServerCommandStatistic('data/cmdCancelledRate.csv', 'data/appUsageCancelled.csv');
+//getCommandDelayAverage('cmd_delay_to_confirmed');
 
-function getAppUsageServerCommandStatistic() {
-    findCommandSuccessRate(function(err, commandSuccessRates) {
-        findAppUsages(commandSuccessRates, function(err, appUsages) {
-           mergeArraysById(commandSuccessRates, appUsages, function(res1, res2) {
-              console.log('Cmd Success Rates\n\n');
-              res1.forEach(function(cmdSr) {
-                  console.log(cmdSr);
-              });
-              console.log('App openings\n\n');
-              res2.forEach(function(appU) {
-                  console.log(appU);
-              });
-           });
+function calcAppUsageServerCommandStatistic(filenameCmdSuccessRate, filenameAppUsage) {
+    getCommandSuccessRate('cmd_delay_to_pos_any', function(err, commandSuccessRates) {
+        if (err) {
+            console.error(err.message);
+            return;
+        }
+        getAppUsages(commandSuccessRates, function (err, appUsages) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            mergeArraysById(commandSuccessRates, appUsages, function (res1, res2) {
+                //console.log('Cmd Success Rates\n\n');
+                writeToCsvFile(res1,filenameCmdSuccessRate);
+                writeToCsvFile(res2, filenameAppUsage);
+                // res1.forEach(function (cmdSr) {
+                //     console.log(cmdSr);
+                // });
+                //console.log('App openings\n\n');
+                //res2.forEach(function (appU) {
+                  //  console.log(appU);
+                //});
+            });
         });
     });
+}
+
+function writeToCsvFile(data, filename, options) {
+    var headers = [];
+    for (var i = 0; i < data.length; i++) {
+        headers.push('val ' + i);
+    }
+    var writer = csvWriter({headers: headers, sendHeaders: false});
+    writer.pipe(fs.createWriteStream(filename, options));
+    writer.write(data);
+    writer.end();
 }
 
 function mergeArraysById(arr1, arr2, cb) {
@@ -27,7 +51,7 @@ function mergeArraysById(arr1, arr2, cb) {
     for (var i = 0; i < arr1.length; i++) {
         for (var j = 0; j < arr2.length; j++) {
             if (arr1[i]._id.toString() === arr2[j]._id.toString()) {
-                resultArr1.push(Math.round(arr1[i].cmdSuccessRate * 1000) / 1000);
+                resultArr1.push(Math.round(arr1[i].cmdDelay * 1000) / 1000);
                 resultArr2.push(arr2[j].appOpenings);
             }
         }
@@ -45,16 +69,14 @@ function getRandomUserIds(cb) {
 }
 
 function queryRandomUserIds(connection, cb) {
-    connection.collection('ppl_subscriptions').aggregate([
-        {'$match': {status: 'ACTIVE', created_at: {$lte: new Date("2017-05-01T00:00:00.000Z")}}},
+    var pipeline = [
+        {'$match': {status: 'ACTIVE', created_at: {$lte: new Date("2017-06-01T00:00:00.000Z")}}},
         {'$group': {_id: '$user_id', countTrackers: {$sum: 1}}},
         {'$match': {countTrackers: 1}},
         {'$skip': Math.random() * 100},
         {'$limit': 5000}
-    ], function (err, result) {
-        connection.close();
-        return cb(err, extractArrayContent(result));
-    });
+    ];
+    db.aggregate(connection,'ppl_subscriptions', pipeline, cb);
 }
 
 function extractArrayContent(jsonArray) {
@@ -84,7 +106,7 @@ function queryClientIds(connection, cb) {
             });
 }
 
-function findAppUsages(commandSuccessRates, cb) {
+function getAppUsages(commandSuccessRates, cb) {
     async.series({
         clientIds: getClientIds
     }, function (err, result) {
@@ -98,13 +120,13 @@ function findAppUsages(commandSuccessRates, cb) {
 }
 
 function queryAppUsages(userIds, clientIds, connection, cb) {
-    connection.collection('mobile_events').aggregate([
+    var pipeline = [
         {
             '$match': {
                 user_id: {$in: userIds},
                 created_at: {
-                    $gte: new Date("2017-05-01T00:00:00.000Z"),
-                    $lt: new Date("2017-06-01T00:00:00.000Z")
+                    $gte: new Date("2017-06-01T00:00:00.000Z"),
+                    $lt: new Date("2017-06-11T00:00:00.000Z")
                 },
                 event: 'app_foreground',
                 client_id: {$in: clientIds}
@@ -112,52 +134,107 @@ function queryAppUsages(userIds, clientIds, connection, cb) {
         },
         {'$group': {_id: '$user_id', appOpenings: {$sum: 1}}},
         {'$match': {appOpenings: {$gte: 1}}}
-    ], function (err, result) {
-        connection.close();
-        return cb(err, result);
-    });
+    ];
+    db.aggregate(connection, 'mobile_events', pipeline, cb);
 }
 
-function findCommandSuccessRate(cb) {
+function getCommandSuccessRate(cb) {
     async.series({
-       userIds: getRandomUserIds
+        userIds: getRandomUserIds
     }, function (err, result) {
         async.waterfall([
             async.apply(db.createConnection, 'tractivedb_metrics'),
-            async.apply(queryCommandSuccessRate, result.userIds)
+            async.apply(queryCommandSuccessRate, extractArrayContent(result.userIds))
         ], function (err, result) {
-            cb(err, result);
+            cb(err ,result);
         });
     });
 }
 
 function queryCommandSuccessRate(userIds, connection, cb) {
     var commands = [
-        "MSG_S2D_POS_REPORT_REQUEST",
-        "MSG_S2D_BUZZER_CONTROL",
-        "MSG_S2D_LED_CONTROL",
         "MSG_S2D_LIVE_TRACKING_MODE"
     ];
-    connection.collection('server_command_metrics').aggregate([
+    var pipeline = [
         {
             '$match': {
                 msg_name: {$in: commands},
                 requested_by: {$in: userIds},
                 created_at: {
-                    $gte: new Date("2017-05-01T00:00:00.000Z"),
-                    $lt: new Date("2017-06-01T00:00:00.000Z")
+                    $gte: new Date("2017-06-01T00:00:00.000Z"),
+                    $lt: new Date("2017-06-11T00:00:00.000Z")
                 }
             }
         },
         {'$group': {_id: '$requested_by', cmdSuccessRate: {$avg: '$cmd_success_rate'}}},
         {'$limit': 1086}
+    ];
+
+    db.aggregate(connection, 'server_command_metrics', pipeline, cb);
+}
+
+function getCommandDelayAverage(cmdDelayType) {
+    find('tractivedb_metrics', queryCommandDelayAverage, cmdDelayType, function(err, result) {
+        if (err) {
+            console.error(err.message);
+        } else {
+            console.log(result);
+        }
+    });
+}
+
+function find(database, queryFunc, queryFuncParameter, cb) {
+    async.waterfall([
+        async.apply(db.createConnection, database),
+        async.apply(queryFunc, queryFuncParameter)
     ], function (err, result) {
-        connection.close();
         cb(err, result);
     });
 }
 
+function queryCommandDelayAverage(cmdDelayType, connection, cb) {
+    var matchCriteria = {};
+    matchCriteria[cmdDelayType] = {'$exists': true};
+    matchCriteria['requested_at'] = {
+        '$gte': new Date("2017-06-01T00:00:00.000Z"),
+        '$lt': new Date("2017-06-11T00:00:00.000Z")
+    };
 
+    var pipeline = [
+        {'$match': matchCriteria},
+        {'$group': {_id: null, cmdDelayAverage: {$avg: '$' + cmdDelayType}}}
+    ];
+    db.aggregate(connection, 'server_command_metrics', pipeline, cb);
+}
+
+function getCommandDelayForUsers(cmdDelayType, cb) {
+    async.series({
+        userIds: getRandomUserIds
+    }, function(err, result) {
+        async.waterfall([
+            async.apply(db.createConnection, 'tractivedb_metrics'),
+            async.apply(queryCommandDelayForUsers, cmdDelayType, extractArrayContent(result.userIds))
+        ], function (err, result) {
+            cb(err, result);
+        });
+    });
+}
+
+function queryCommandDelayForUsers(cmdDelayType, userIds, connection, cb) {
+    var matchCriteria = {};
+    matchCriteria[cmdDelayType] = {'$exists': true};
+    matchCriteria['requested_by'] = {'$in': userIds};
+    matchCriteria['created_at'] = {
+        '$gte': new Date("2017-06-01T00:00:00.000Z"),
+        '$lt': new Date("2017-06-11T00:00:00.000Z")
+    };
+    var pipeline = [
+        {'$match': matchCriteria},
+        {'$group': {_id: '$requested_by', cmdDelay: {$avg: '$' + cmdDelayType}}},
+        {'$limit': 1086}
+    ];
+    db.aggregate(connection, 'server_command_metrics', pipeline, cb);
+}
 
 
 
