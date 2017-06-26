4e0d0bf73593a36064da7394c667bcb4149c6e93
Jürgen Ratzenböck <jur@tractive.com>
2017-06-26 10:19:34 +0200

    Refactor index.js file to manage correlation calculations more efficiently

--- src/index.js
+++ src/index.js
@@ -5,52 +5,99 @@ var util = require('./util');
 var serverCmdMetrics = require('./server-cmd-metrics');
 var appUsage = require('./app-usage');
 var clients = require('./clients');
+var users = require('./users');
+var dvcHwMetrics = require('./dvc-hw-metrics');
 
 var commands = [
+    'MSG_S2D_BUZZER_CONTROL',
+    'MSG_S2D_ED_CONTROL',
     'MSG_S2D_LIVE_TRACKING_MODE'
 ];
-var startDate = new Date("2017-06-01T00:00:00.000Z");
-var endDate = new Date("2017-06-11T00:00:00.000Z");
+var startDate = new Date('2017-06-01T00:00:00Z');
+var endDate = new Date('2017-06-19T00:00:00Z');
 
-var filenames = ['data/cmdCancelledRate.csv', 'data/appUsageCancelled.csv'];
-cmdTerminatedRateVsAppUsage();
+var filenames = ['data/errLogCount.csv', 'data/appUsageErrLogCount.csv'];
+cmdSuccessRateVsAppUsage();
+//cmdTerminatedRateVsAppUsage(filenames);
 //serverCmdMetrics.getServerCommandAverage(commands, 'cmd_delay_to_confirmed', startDate, endDate);
+//batteryCriticalEventsVsAppUsage();
+//systemShutDownEventsVsAppUsage();
+//errLogCountVsAppUsage(filenames);
 
 function cmdSuccessRateVsAppUsage(filenames) {
     calcServerCmdMetricVsAppUsage(commands, 'cmd_success_rate', filenames);
 }
 
 function cmdCancelledRateVsAppUsage(filenames) {
-    calcServerCmdMetricVsAppUsage(commands, 'cmd_cancelle_rate', filenames);
+    calcServerCmdMetricVsAppUsage(commands, 'cmd_cancelled_rate', filenames);
 }
 
 function cmdTerminatedRateVsAppUsage(filenames) {
     calcServerCmdMetricVsAppUsage(commands, 'cmd_terminated_rate', filenames);
 }
 
+function batteryCriticalEventsVsAppUsage(filenames) {
+    calcHwStatusMetricVsAppUsage(['BATT_CHARGING'], filenames);
+}
+
+function systemShutDownEventsVsAppUsage(filenames) {
+    calcHwStatusMetricVsAppUsage(['SYSTEM_SHUTDOWN'], filenames);
+}
+
+function errLogCountVsAppUsage(filenames) {
+    calcErrorLogCountVsAppUsage(filenames);
+}
+
 function calcServerCmdMetricVsAppUsage(commands, cmdStatistic, filenames) {
-    var commandRateType = 'cmd_success_rate';
     serverCmdMetrics.getServerCommandMetricsForUsers(
         commands, cmdStatistic, startDate,
         endDate, 1086,
-        function (err, cmdStatistic) {
+        function (err, cmdStatResult) {
             if (err) {
                 console.error(err.message);
                 return;
             }
-            var userIds = util.getObjectIdsAsStringArray(cmdStatistic);
-            appUsage.getAppUsages(userIds, clients.getClientIds, startDate, endDate, function (err, appUsages) {
-                if (err) {
-                    console.error(err.message);
-                    return;
-                }
-                util.mergeArraysById(cmdStatistic, appUsages, _.camelCase(commandRateType), 'appOpenings', function (res1, res2) {
+            mergeAppUsage(cmdStatResult, _.camelCase(cmdStatistic), filenames);
+        });
+}
+
+function calcHwStatusMetricVsAppUsage(hwStatus, filenames) {
+    dvcHwMetrics.getHwStatusMetricsForUsers(
+        hwStatus, startDate,
+        endDate, 1086,
+        function (err, hwStatusResult) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            mergeAppUsage(hwStatusResult, _.camelCase(hwStatus + '_average'), filenames);
+        });
+}
+
+function calcErrorLogCountVsAppUsage(filenames) {
+    dvcHwMetrics.getErrorLogCount(startDate, endDate, 1086, function(err, errLogCount) {
+       if (err) {
+           console.error(err.message);
+           return;
+       }
+       mergeAppUsage(errLogCount, 'sum_err_log_count', filenames);
+    });
+}
+
+function mergeAppUsage(firstResult, firstResultFieldName, filenames) {
+    appUsage.getAppUsages(util.getObjectIdsAsStringArray(firstResult, '_id'), clients.getClientIds,
+        startDate, endDate, function (err, appUsages) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            util.mergeArraysById(firstResult, appUsages, firstResultFieldName, 'appOpenings',
+                function (res1, res2) {
                     if (!!filenames) {
                         util.writeResultsToFile(res1, res2, filenames[0], filenames[1]);
                     } else {
                         util.writeResultsToConsole(res1, res2);
                     }
                 });
-            });
         });
 }
