68a9fcbe8cb08523a28caaa3fdeedd5881341250
Jürgen Ratzenböck <jur@tractive.com>
2017-06-26 10:18:56 +0200

    Get users for tracker ids

--- src/users.js
+++ src/users.js
@@ -1,7 +1,10 @@
 var async = require('async');
 var db = require('./db');
 
-module.exports.getRandomUserIds = getRandomUserIds;
+module.exports = {
+    getRandomUserIds: getRandomUserIds,
+    getUserIdsForTrackerIds: getUserIdsForTrackerIds
+};
 
 function getRandomUserIds(createdBefore, cb) {
     async.waterfall([
@@ -21,4 +24,21 @@ function queryRandomUserIds(createdBefore, connection, cb) {
         {'$limit': 5000}
     ];
     db.aggregate(connection, 'ppl_subscriptions', pipeline, cb);
+}
+
+function getUserIdsForTrackerIds(trackerIds, cb) {
+    async.waterfall([
+        async.apply(db.createConnection, 'tractivedb'),
+        async.apply(queryUserIdsForTrackerIds, trackerIds)
+    ], function (err, result) {
+        cb(err, result);
+    });
+}
+
+function queryUserIdsForTrackerIds(trackerIds, connection, cb) {
+    var pipeline = [
+        {'$match': {status: 'ACTIVE', tracker_id: {$in: trackerIds}}},
+        {'$group': {'_id': '$user_id'}}
+    ];
+    return db.aggregate(connection, 'ppl_subscriptions', pipeline, cb);
 }
