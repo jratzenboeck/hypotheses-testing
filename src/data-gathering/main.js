2d1c6b6516cfba052627f3f18921f0e6d6cc0178
Jürgen Ratzenböck <jur@tractive.com>
2017-10-08 09:33:35 +0200

    Use all data retrieval methods + update package.json

--- src/data-gathering/main.js
+++ src/data-gathering/main.js
@@ -5,9 +5,13 @@ var idReport = require('./id-report-data');
 var posReport = require('./pos-report-data');
 var serverCmd = require('./server-cmd-data');
 var subscription = require('./subscription-data');
+var device = require('./device-data');
 var user = require('./user-data');
 var customerSupport = require('./customer-support-data');
 var util = require('../util');
+var dvcErrors = require('./dvc-err-report-data');
+var geofence_report_data = require('./geofence-report-data');
+var geofence_data = require('./geofence-data');
 
 insertCustomerSurveyData();
 
@@ -17,24 +21,29 @@ function insertCustomerSurveyData() {
         expandData
     ], function (err, finalResultData) {
         if (err) {
-            console.log('An error occurred, message: ' + err.message);
+            console.log('An error occurred, message: ' + err.message + ', full error: ' + err);
         } else {
             console.log(JSON.stringify(finalResultData));
             console.log(finalResultData.length);
-            var headers = _.keys(_.first(_.filter(finalResultData, function(data) { return data.ticket_count > 0})));
-            util.writeDataToFile(finalResultData, headers, 'ml_data/test_data.csv');
+            var headers = initCsvHeaderData(finalResultData);
+            util.writeDataToFile(finalResultData, headers, 'ml_data/ML_Data_new.csv');
         }
     });
 }
 
+function initCsvHeaderData(dataRows) {
+    return _.keys(_.omit(_.head(dataRows), ['zendesk_user']));
+}
+
 function expandData(customerSurveyData, cb) {
     async.waterfall([
-        async.apply(user.insertUserEmail, customerSurveyData),
-        subscription.insertStartDate,
-        customerSupport.insertCustomerSupportData
-        // posReport.insertPosReportData,
-        // idReport.insertNumberOfDaysInUse,
-        // serverCmd.insertServerCmdMetricsData
+        async.apply(geofence_data.insertGeofenceData, customerSurveyData),
+        subscription.insertSubscriptionData,
+        //device.insertDeviceData,
+        //customerSupport.insertCustomerSupportData,
+        posReport.insertPosReportData,
+        idReport.insertNumberOfDaysInUse,
+        serverCmd.insertServerCmdMetricsData
     ], cb);
 }
 
