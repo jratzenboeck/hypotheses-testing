f5b1241d1e687f27e8adb56245fa932f6810e75b
Jürgen Ratzenböck <jur@tractive.com>
2017-10-14 11:30:14 +0200

    Add pet survey data

---
+++ src/data-gathering/pet-survey-data.js
@@ -0,0 +1,75 @@
+var async = require('async');
+var util = require('../util');
+var db = require('../db');
+var _ = require('lodash');
+var utility = require('./utility');
+
+var COLLECTION_ANSWERS = 'pet_survey_answers';
+var COLLECTION_QUESTIONS = 'pet_survey_questions';
+
+module.exports = {
+    insertPetSurveyData: insertPetSurveyData
+};
+
+function insertPetSurveyData(dataInstances, cb) {
+    console.log('Fetching pet survey data for users...');
+    utility.insertAverageStatisticValueUser(dataInstances, getPetSurveyDataForUser, cb);
+}
+
+function getPetSurveyDataForUser(data, cb) {
+    initPetQuestionHashMap(function(err, questionHashMap) {
+        if (err) {
+            cb(err, null);
+        }
+        async.waterfall([
+            async.apply(initPetSurveyData, data.user_id),
+            async.apply(queryPetSurveyAnswersForUser, data.user_id, data.submit_date, questionHashMap)
+        ], cb);
+    })
+}
+
+function initPetSurveyData(userId, cb) {
+    queryPetSurveyQuestions(function(err, questions) {
+        if (err) {
+            return cb(err, questions);
+        }
+        var petSurveyData = {_id: userId};
+        _.forEach(questions, function(question) {
+            petSurveyData[question.text.en] = -1;
+        });
+        return cb(err, petSurveyData);
+    })
+}
+
+function queryPetSurveyQuestions(cb) {
+    return db.find(db.getTractiveDbConnection(), COLLECTION_QUESTIONS, {}, {_id: 1, text: 1}, {}, cb);
+}
+
+function initPetQuestionHashMap(cb) {
+    var hashMap = [];
+
+    queryPetSurveyQuestions(function(err, questions) {
+        if (err) {
+            return cb(err, questions);
+        }
+        _.forEach(questions, function(question) {
+            hashMap.push({_id: question._id.toString(), text: question.text.en});
+        });
+        cb(err, hashMap);
+    });
+}
+
+function queryPetSurveyAnswersForUser(userId, submitDate, petSurveyQuestions, petSurveyData, cb) {
+    return db.find(db.getTractiveDbConnection(), COLLECTION_ANSWERS, {user_id: userId, created_at: {$lte: submitDate}}, {}, {}, function(err, answers) {
+        if (err) {
+            return cb(err, answers);
+        }
+        _.forEach(answers, function(answer) {
+            var questionText = _.first(_.filter(petSurveyQuestions, function(q) {
+                return q._id === answer.question_id.toString();
+            })).text;
+            petSurveyData[questionText] = answer.given_answer_id.toString();
+        });
+        cb(err, petSurveyData);
+    });
+}
