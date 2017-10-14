26aec7dcae63b93acd5779280a7420ec636388f7
Jürgen Ratzenböck <jur@tractive.com>
2017-10-14 11:29:52 +0200

    Add possibility to only find customer survey data for users who filled in survey once

--- src/data-gathering/customer-survey-data.js
+++ src/data-gathering/customer-survey-data.js
@@ -4,18 +4,66 @@ var db = require('../db');
 var COLLECTION = 'user_survey_answers';
 
 module.exports = {
-    getUserSurveyData: getUserSurveyData
+    getUserSurveyDataAll: getUserSurveyDataAll,
+    getUserSurveyDataUnique: getUserSurveyDataUnique
 };
 
-function getUserSurveyData(cb) {
+function getUserSurveyData(func, cb) {
     async.series([
         db.createTractiveDbConnection,
-        findUserSurveyData
+        func
     ], function(err, result) {
         cb(err, result[1]);
     });
 }
 
+function getUserSurveyDataUnique(cb) {
+    getUserSurveyData(findUserSurveyDataUnique, cb);
+}
+
+function getUserSurveyDataAll(cb) {
+    getUserSurveyData(findUserSurveyData, cb);
+}
+
 function findUserSurveyData(cb) {
-    return db.find(db.getTractiveDbConnection(), COLLECTION, {tracker_id: {$exists: true}}, {_id: 0, submit_date: 1, rating: 1, recommendation_score: 1, user_id: 1, tracker_id: 1}, {}, cb);
+    return db.find(db.getTractiveDbConnection(), COLLECTION, {tracker_id: {$exists: true}},
+        {_id: 0, submit_date: 1, rating: 1, recommendation_score: 1, user_id: 1, tracker_id: 1}, {}, cb);
+}
+
+function findUserSurveyDataUnique(cb) {
+    var pipeline = [
+        {
+            '$match': {
+                tracker_id: {$exists: true}
+            }
+        },
+        {
+            '$group': {
+                _id: '$user_id', count: {$sum: 1},
+                survey_data: {
+                    '$push': {
+                        submit_date: '$submit_date', rating: '$rating',
+                        recommendation_score: '$recommendation_score',
+                        user_id: '$user_id', tracker_id: '$tracker_id'
+                    }
+                }
+            }
+        },
+        {
+            '$match': {
+                count: 1
+            }
+        },
+        {
+            '$unwind': '$survey_data'
+        },
+        {
+            '$project': {
+                _id: 0, submit_date: '$survey_data.submit_date', rating: '$survey_data.rating',
+                recommendation_score: '$survey_data.recommendation_score', user_id: '$survey_data.user_id',
+                tracker_id: '$survey_data.tracker_id'
+            }
+        }
+    ];
+    return db.aggregate(db.getTractiveDbConnection(), COLLECTION, pipeline, cb);
 }
